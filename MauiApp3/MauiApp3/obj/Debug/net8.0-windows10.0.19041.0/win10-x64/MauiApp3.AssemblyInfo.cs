2fa330f43598e5c3ba52f77ebcc46c4b9ddadad1
xalmasid <55780735+xalmasid@users.noreply.github.com>
2024-02-26 19:50:16 +0100

    MAUI

--- MauiApp3/MauiApp3/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/MauiApp3.AssemblyInfo.cs
+++ MauiApp3/MauiApp3/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/MauiApp3.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+1b7cd08de68f91f12789aeecd1fb0e61351eaba5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+6fb5bb838f7445730f465797b0aa174eb8ddd720")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
