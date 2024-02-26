6fb5bb838f7445730f465797b0aa174eb8ddd720
Darlex56 <danielalmasi24@gmail.com>
2024-02-26 15:46:00 +0100

    MAUI

---
+++ MauiApp3/MauiApp3/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using CommunityToolkit.Maui.Views;
+
+namespace MauiApp3
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void onPlayClicked(object sender, EventArgs e) { 
+            mediaPlayer.Play();
+        }
+        public void onPauseClicked(object sender, EventArgs e)
+        {
+            mediaPlayer.Pause();
+        }
+    }
+
+}
